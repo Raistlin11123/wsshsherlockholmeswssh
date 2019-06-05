2bf1e06b95235267aada960c02e86ffbc67438e2
Raistlin11123 <37593164+Raistlin11123@users.noreply.github.com>
2019-06-05 11:53:25 +0200

    MAJ admin + title responsive

--- investigation1/admin.py
+++ investigation1/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import Profil, Clue, UserClues
 # Register your models here.
 
+class ClueAdmin(admin.ModelAdmin):
+    list_display   = ('title', 'number',)
+    ordering       = ('title', 'number')
+
+class UserCluesAdmin(admin.ModelAdmin):
+    list_display   = ('player', 'clue',)
+    list_filter    = ('player', 'clue')
+    ordering       = ('player', 'clue')
+    search_fields  = ('player', 'clue')
+
+
 admin.site.register(Profil)
-admin.site.register(Clue)
-admin.site.register(UserClues)
+admin.site.register(Clue, ClueAdmin)
+admin.site.register(UserClues, UserCluesAdmin)
+
+#Ajouter des tris
